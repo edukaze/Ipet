91baed9bd8b54e37ed83c67220476aa7e4352faa
tiago144 <tiagobarros37@hotmail.com>
2020-10-20 21:43:09 -0300

    finalizando a validação da ong

--- verificar_cadastro_ong.php
+++ verificar_cadastro_ong.php
@@ -1,21 +1,33 @@
-<?php
-include "banco.php";
+$pdo = dbConnect();
 
-$cnpj = $_POST['o-cnpj'];
-$usuario = $_POST['o-usuario'];
-$facebook = $_POST['o-facebook'];
-$instagram = $_POST['o-instagram'];
-$email = $_POST['o-email'];
-$telefone = $_POST['o-telefone'];
-$nome = $_POST['o-nome-ong'];
-$senha = $_POST['o-senha'];
-$conf_senha = $_POST['o-conf_senha']
-$descricao = $_POST['o-descricao'];
+$verificandoExistencia = $pdo->prepare("
+		SELECT COUNT(*) AS TOTAL FROM IPET_USUARIO_ONG
+		WHERE ONG_USUARIO = ?;
+	");
+$verificandoExistencia->execute([$usuario]);
+$row = $verificandoExistencia->fetchAll();
 
-if (empty($cnpj) ||  empty($usuario) || empty($facebook) || empty($instagram) || empty($email) || empty($telefone) || empty($nome)
-|| empty($senha) || empty($conf_senha) || empty($descrição)){
-	$_SESSION['erro-campo'] = true;
-	header("location:cadastro_ongs.php");
-	exit(); 
+
+	if($row[0]['TOTAL'] == 1) {
+	$_SESSION['usuario_existe'] = true;
+	header('Location: cadastro_ongs.php');
+	exit();
 }
-   
+elseif ($row[0]['TOTAL']  != 1) {
+	
+$stmt = $pdo->prepare("
+    INSERT INTO ipet_usuarios_ong (ONG_CNPJ, ONG_USUARIO, ONG_FACEBOOK, ONG_INSTAGRAM, ONG_EMAIL, ONG_TELEFONE, ONG_NOME, ONG_SENHA, ONG_CONF_SENHA, ONG_DESCRICAO)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+");
+$stmt->execute([$cnpj, $usuario, $facebook, $instagram, $email, $telefone, $nome, $senha, $conf_senha, $descricao]);
+
+	$_SESSION['status_cadastro'] = true;
+	header('location: cadastro_ongs.php');
+
+	exit;
+
+}
+
+
+
+?>
