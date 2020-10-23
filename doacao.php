ecaf896a8b8c67957fc8a0452174257e1340660e
desireefernandes <desireefernandes00@gmail.com>
2020-10-23 02:56:16 -0300

    Update restrição de acesso doação.php

--- doacao.php
+++ doacao.php
@@ -1,5 +1,13 @@
 <?php 
 session_start();
+
+if (isset($_SESSION['anonimo'])) {
+    header("location:login.php");
+}
+    elseif (isset($_SESSION['anonimo'])) {
+    header("location:login.php");
+}
+
 ?>
 
 <!DOCTYPE html>
