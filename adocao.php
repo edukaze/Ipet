323e0cd953400136a6980adf6b8eb346001b2f9e
cleicykerolim <cleicykerolim@gmail.com>
2020-10-23 01:14:12 -0300

    Add a página adoção

--- adocao.php
+++ adocao.php
@@ -26,12 +26,8 @@ $rowTotal = $stmt->rowCount();
 <html>
 <head>
 	<title>Adote</title>
-	<style type="text/css">
-		
-		.animais{
-			border:1px solid black;
-		}
-	</style>
+	<link rel="stylesheet" type="text/css" href="css/cadastro_adocao.css">
+	
 </head>
 <body>
 
@@ -47,5 +43,59 @@ $rowTotal = $stmt->rowCount();
 			</div>
 <?php endforeach; ?>
 	<?php endif; ?>
+
+	<!-- navbar -->
+	<header id="header">
+		<a href="" class="logo"><img src="img/iPettt.png"></a>
+		<ul>
+			<li><a href="index.php" onclick="toggle()">Home</a></li>
+			<li><a href="index.php"  onclick="toggle()">Sobre</a></li>
+			<li><a href="index.php"  onclick="toggle()">serviços</a></li>
+			<li><a href="index.php"  onclick="toggle()">Pets</a></li>
+			<li><a href="index.php"  onclick="toggle()">Equipe</a></li>
+			<!-- <li><a href="#contato"  onclick="toggle()">Contato</a></li> -->
+			<li><a href="adocao.php"  onclick="toggle()">Adoção</a></li>
+			<li><a href="doacao.php"  onclick="toggle()">Doação</a></li>
+			<li><a href="#"  onclick="toggle()">Ongs</a></li>
+			<?php if(isset($_SESSION['nome'])): ?>
+			<li><a href="index.php"  onclick="toggle()" class="cadastro_user"><?php echo $_SESSION['nome'];?> </a>
+		<ul>
+			<li><a href="sair.php"  onclick="toggle()" class="cadastro_user">sair</a></li>
+		</ul>
+			</li>
+				<?php elseif (isset($_SESSION['nome_ong'])): ?>
+				<li><a href="index.php"  onclick="toggle()" class="cadastro_user"><?php echo $_SESSION['nome_ong']; ?></a>
+					<ul>
+						<li><a href="sair.php"  onclick="toggle()" class="cadastro_user">sair</a></li>
+					</ul>
+				</li>
+				<?php else: ?>
+					<?php unset($_SESSION['nome']); ?>
+					<?php unset($_SESSION['nome_ong']); ?>
+					<li><a href="login.php"  onclick="toggle()" class="cadastro">cadastro</a></li>
+				<?php endif ?>
+		</ul>
+		<div class="toggle" onclick="toggle()"></div>
+	</header>
+
+		<!-- banner -->
+	<section class="banner" id="home">
+		<div class="overlay">
+			<!-- <h2><span>Atitude é uma pequena<br>coisa que faz uma grande diferença</span></h2> -->
+		</div>
+	</section>
+
+	<script type="text/javascript">
+		// Deixa o header fixo no site
+		window.addEventListener("scroll", function(){
+			var header = document.querySelector("header");
+			header.classList.toggle("sticky", window.scrollY > 0);
+		})
+		// Essa função é para o menu responsivo
+		function toggle(){
+			var header = document.querySelector("header");
+			header.classList.toggle("active");
+		}
+	</script>
 </body>
 </html>
--- doacao.php
+++ doacao.php
@@ -1,12 +1,5 @@
 <?php 
 session_start();
-
-if (isset($_SESSION['anonimo'])) {
-	header("location:login.php");
-}
-	elseif (isset($_SESSION['anonimo'])) {
-	header("location:login.php");
-}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
