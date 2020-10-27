524034e9f6f86e4122519cc4e4028917205593e7
Suelen Salvino <suelensalvinocs@gmail.com>
2020-10-27 02:29:12 -0300

    iniciando processo de update

--- edicao_dados.php
+++ edicao_dados.php
@@ -10,25 +10,27 @@ $genero = $_POST['a-genero'];
 $descricao = $_POST['a-descricao'];
 
 $pdo = dbConnect();
-$query = "SELECT * FROM IPET_ANIMAIS WHERE ANI_CODIGO";
+$query = "SELECT * FROM IPET_ANIMAIS LEFT JOIN IPET_USUARIOS_ONG ON ANI_ONG_ID = ONG_ID;";
 
 $stmt = $pdo->prepare($query);
 
 $stmt->execute();
-$animais =  $stmt->fetchAll();
+$chaveAni = $stmt->fetchAll();
+
+$_SESSION['ani-codigo'] = $chaveAni[0]['ANI_CODIGO'];
+$_SESSION['ani-nome'] = $chaveAni[0]['ANI_NOME'];
+
+var_dump($chaveAni);
 
 
- //var_dump($animais);
-$_SESSION['ani_codi'] = $animais[0]['ANI_CODIGO'];
-var_dump($_SESSION['ani_codi']);
-
-$query = "UPDATE IPET_ANIMAIS SET ANI_NOME=?, ANI_ESPECIE=?, ANI_RACA=?, ANI_PORTE=?, ANI_GENERO=?, ANI_DESCRICAO=? WHERE ANI_CODIGO=?";
+$query = "UPDATE IPET_ANIMAIS SET ANI_NOME=?, ANI_ESPECIE=?, ANI_RAÇA=?, ANI_PORTE=?, ANI_GENERO=?, ANI_DESCRICAO=? WHERE ANI_CODIGO=? AND ANI_NOME=?";
 
 $stmt = $pdo->prepare($query);
 
-$stmt->execute([$nome, $especie, $raca, $porte, $genero, $descricao, $_SESSION['ani_codi']]);
+$stmt->execute([$nome, $especie, $raca, $porte, $genero, $descricao, $_SESSION['ani-codigo'], $_SESSION['ani-nome']]);
 
+unset($_SESSION['ani-codigo']);
 print_r($stmt->errorInfo());
-header('location:adocao.php');
+//header('location:adocao.php');
 
 ?>
