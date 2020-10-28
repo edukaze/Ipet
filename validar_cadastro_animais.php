5554fd8bd5551f0434700c8d631bf37fa379ae7f
edukaze <eduardo.bezerra.kaze@gmail.com>
2020-10-27 21:09:51 -0300

    adicionando a chave estrangeira de animais e especies

--- validar_cadastro_animais.php
+++ validar_cadastro_animais.php
@@ -10,6 +10,28 @@ $porte = $_POST['a-porte'];
 $genero = $_POST['a-genero'];
 $descricao = $_POST['a-descricao'];
 $nome_imagem = $_FILES['imagem']['name'];
+
+$query =  $pdo->prepare("
+	SELECT ESP_ID FROM IPET_ESPECIE
+	");
+$query ->execute();
+$chaveEspecie = $query->fetchAll();
+var_dump($especie);
+var_dump($chaveEspecie);
+
+	if ($especie = "cachorro") {
+		$insert =  $pdo->prepare("
+			INSERT INTO IPET_ESPECIE(ESP_CACHORRO)
+			VALUES (?);
+			");
+		$insert ->execute([$especie]);
+		$select = $pdo->prepare("
+			SELECT ESP_ID, ESP_CACHORRO FROM IPET_ESPECIE
+			where esp_cachorro= ;
+			")
+	}
+
+
 include 'funcoes.php';
 
 if (empty($nome) ||  empty($especie) || empty($raca) || empty($porte) || empty($genero) || empty($descricao)){
@@ -26,11 +48,11 @@ $chaveUsu = $_SESSION['id_usuario'];
 $ultimo_id = $chaveUsu;
 	
 $stmt = $pdo->prepare("
-    INSERT  INTO IPET_ANIMAIS (ANI_NOME, ANI_ESPECIE, ANI_RAÇA, ANI_PORTE, ANI_GENERO, ANI_DESCRICAO, ANI_NOR_CODIGO, ANI_IMAGEM)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+    INSERT  INTO IPET_ANIMAIS (ANI_NOME, ANI_ESPECIE, ANI_RAÇA, ANI_PORTE, ANI_GENERO, ANI_DESCRICAO, ANI_NOR_CODIGO, ANI_IMAGEM, ANI_ESP_ID)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?,?);
 ");
 
-$stmt->execute([$nome, $especie, $raca, $porte, $genero, $descricao, $chaveUsu,$nome_imagem]);
+$stmt->execute([$nome, $especie, $raca, $porte, $genero, $descricao, $chaveUsu,$nome_imagem, $chaveEspecie]);
 
 include 'validar_imagem.php';
 print_r($stmt->errorInfo());
@@ -40,11 +62,11 @@ header("location:adocao.php");
 $chaveOng = $_SESSION['id_ong'];
 $ultimo_id = $chaveOng;	
 $stmt = $pdo->prepare("
-    INSERT  INTO IPET_ANIMAIS (ANI_NOME, ANI_ESPECIE, ANI_RAÇA, ANI_PORTE, ANI_GENERO, ANI_DESCRICAO, ANI_ONG_ID, ANI_IMAGEM)
-    VALUES (?, ?, ?, ?, ?, ?, ?);
+    INSERT  INTO IPET_ANIMAIS (ANI_NOME, ANI_ESPECIE, ANI_RAÇA, ANI_PORTE, ANI_GENERO, ANI_DESCRICAO, ANI_ONG_ID, ANI_IMAGEM, ANI_ESP_ID)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
 ");
 
-$stmt->execute([$nome, $especie, $raca, $porte, $genero, $descricao, $chaveOng, $nome_imagem]);
+$stmt->execute([$nome, $especie, $raca, $porte, $genero, $descricao, $chaveOng, $nome_imagem, $chaveEspecie]);
 include 'validar_imagem.php';
 print_r($stmt->errorInfo());
 header("location:adocao.php");
