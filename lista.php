9c95b1f4fa2d0eb1d30d7e0240501248f3f8c098
Billynhoo <lamartinenogueira7@gmail.com>
2020-10-30 08:46:33 -0300

    nova lista

--- lista.php
+++ lista.php
@@ -5,21 +5,30 @@
 </head>
 <body>
 
-</body>
-</html>
+
 
 <?php 
+
 include 'banco.php';
  $r = "SELECT * FROM IPET_USUARIOS_ONG ORDER BY ONG_NOME ASC"; 
 
 $resultado= $pdo->prepare($r); 
 $resultado->execute();
 
-while ($a = $resultado ->  fetch(PDO::FETCH_ASSOC)) {
-echo "NOME:". $a['ONG_NOME'] . "<br>";
-echo "DESCRIÇÃO:".$a['ONG_DESCRICAO']."<br>";
+//while ($a = $resultado ->  fetch(PDO::FETCH_ASSOC)) {
+//echo "NOME:". $a['ONG_NOME'] . "<br>";
+//echo "Descrição:".$a['ONG_DESCRICAO']."<br>"; 	
 
-
-}
+//}
 
  ?>
+<table>
+	<th>ONG'S</th>
+	<?php foreach ($resultado as $result):?>
+
+	<td><?=$result['ONG_NOME']?><a href="ong.php<?$go['ONG_ID']?>"></a></td>
+	<td><?=$result['ONG_DESCRICAO']?></td>
+</table>
+      
+</body>
+</html>
