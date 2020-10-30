58e213f4d0dfa52d6df445252f7eaa351f98d28b
Billynhoo <lamartinenogueira7@gmail.com>
2020-10-29 23:18:07 -0300

    listar as ongs

---
+++ lista.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ONG'S </title>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php 
+include 'banco.php';
+ $r = "SELECT * FROM IPET_USUARIOS_ONG ORDER BY ONG_NOME ASC"; 
+
+$resultado= $dsn->prepare($r); 
+$resultado->execute();
+
+while ($a = $resultado ->  fetch(PDO::FETCH_ASSOC)) {
+echo "NOME:". $a['ONG_NOME'] . "<br>";
+ 	
+
+}
+
+ ?>
