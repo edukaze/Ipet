8b4de975b5e0496e3dcccc18c8489570c8054bb2
Suelen Salvino <suelensalvinocs@gmail.com>
2020-11-08 22:50:39 -0300

    Ajustes em cards no perfil

--- perfil.php
+++ perfil.php
@@ -34,6 +34,9 @@ $rowTotal = $stmt->rowCount();
 	<title>Perfil</title>
 	<link rel="stylesheet" type="text/css" href="css/edicao.css"/>
 	<link rel="icon" type="imagem/png" href="/img/iPettt.png" />
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
 </head>
 <body>
 	<!-- navbar -->
@@ -43,48 +46,63 @@ $rowTotal = $stmt->rowCount();
 				<h1>MEU(S) PET(S)</h1>
 			</section>
 
-			<h3>altera informações do perfil <a href="alterar-perfil.php?id=<?= $_SESSION['id_usuario']?>">Alterar</a></h3>
+			<h3>Alterar informações do perfil <a href="alterar-perfil.php?id=<?= $_SESSION['id_usuario']?>">Alterar</a></h3>
 			<section class="edc">
 				<div class="box">
 					<div class="box1">
 
-						<?php if ($rowTotal > 0 && isset($_SESSION['id_usuario'])): ?>
-							<?php foreach ($animais as  $animal): ?>
-								<?php
-								$imagem = 'imagens/1/bbb.jpeg';
-								if (is_file("imagens/" .  $animal['ANI_CODIGO'] . "/" . $animal['ANI_IMAGEM'])) {
-									$imagem = "imagens/" .  $animal['ANI_CODIGO'] . "/" . $animal['ANI_IMAGEM'];
-								}
-								?>
-								<div class="box2">
-									<dl>
-										<dt>Chave Normal usu</dt>
-										<dd><?= $animal['ANI_NOR_CODIGO']?></dd>
-										<dt>Nome</dt>
-										<dd><?= $animal['ANI_NOME']?></dd>
-										<dd><img src="<?= $imagem ?>"></dd>
-										<dt>Espécie</dt>
-										<dd><?= $animal['ANI_ESPECIE']?></dd>
-										<dt>Raça</dt>
-										<dd><?= $animal['ANI_RAÇA']?></dd>
-										<dt>Porte</dt>
-										<dd><?= $animal['ANI_PORTE']?></dd>
-										<dt>Gênero</dt>
-										<dd><?= $animal['ANI_GENERO']?></dd>
-										<dt>Descrição</dt>
-										<dd><?= $animal['ANI_DESCRICAO']?></dd>
-									</dl>
-									<br>
-									<a href="edicao_animal.php?id=<?= $animal['ANI_CODIGO'] ?>">Editar</a>
-									<a href="delete.php?id=<?= $animal['ANI_CODIGO'] ?>" style="background: red;">Excluir</a>
-								</div>
-								
+	<?php if ($rowTotal > 0): ?>
+		<?php foreach ($animais as  $animal): ?>
+			<?php
+			$imagem = 'imagens/1/bbb.jpeg';
+			if (is_file("imagens/" .  $animal['ANI_CODIGO'] . "/" . $animal['ANI_IMAGEM'])) {
+				$imagem = "imagens/" .  $animal['ANI_CODIGO'] . "/" . $animal['ANI_IMAGEM'];
+			}
+			$stmt2 = $pdo->prepare("SELECT COUNT(*) AS total FROM IPET_LIKE WHERE LIK_ANI_CODIGO = ?");
 
-							<?php endforeach; ?>
-						<?php endif; ?>
+			$stmt2->execute([$animal['ANI_CODIGO']]);
+			$likes =  $stmt2->fetch();
+			?>
 
+				<div class="ui card">
+					<div class="image">
+						<img src="<?= $imagem ?>">
 					</div>
-				</div>
+
+					<div class="content">
+						<div class="header"><?= $animal['ANI_NOME']?></div>
+						<div class="summary">
+							Responsável:
+							<?php if ($animal['ANI_NOR_CODIGO'] != null): ?>
+								<a href="perfil-usuario.php?id=<?= $animal['NOR_CODIGO'] ?>"><?= $animal['NOR_NOME']?></a>
+								<?php elseif($animal['ANI_ONG_ID'] != null): ?>
+									<a href="perfil-ong-geral.php?id=<?= $animal['ONG_ID'] ?>"><?= $animal['ONG_NOME']?></a>
+								<?php endif ?>
+							</div>
+					</div>
+
+						<div class="extra content">
+							<div class="description">
+								DESCRIÇÃO <br>
+								Espécie: <?= $animal['ESP_ESPECIE']?> <br>
+								Raça: <?= $animal['ANI_RAÇA']?> <br>
+								Porte: <?= $animal['ANI_PORTE']?> | Gênero: <?= $animal['ANI_GENERO']?> <br>
+							</div>
+						</div>
+
+							<div class="extra content">
+								<div class="descriptin">
+									Sobre o pet: <?= $animal['ANI_DESCRICAO']?>
+								</div>
+							</div>
+							<a href="edicao_animal.php?id=<?= $animal['ANI_CODIGO'] ?>">EDITAR<a href="delete.php?id=<?= $animal['ANI_CODIGO'] ?>">EXCLUIR</a>
+
+				</div><!-- .ui.card -->
+
+		<?php endforeach; ?>
+	<?php endif; ?>
+
+</div><!-- .ui.four -->
 			</section>
 			<?php include 'footer.php'; ?>
 			
--- perfil_ong.php
+++ perfil_ong.php
@@ -13,9 +13,9 @@ $pdo = dbConnect();
 
 
 $stmt = $pdo->prepare("
-	 SELECT * FROM IPET_ANIMAIS
-    LEFT JOIN IPET_USUARIOS_ONG ON ANI_ONG_ID = ONG_ID
-     WHERE ONG_ID = ?;
+	SELECT * FROM IPET_ANIMAIS
+	LEFT JOIN IPET_USUARIOS_ONG ON ANI_ONG_ID = ONG_ID
+	WHERE ONG_ID = ?;
 	");
 
 $stmt->execute([$_SESSION['id_ong']]);
@@ -32,58 +32,75 @@ $rowTotal = $stmt->rowCount();
 	<title>Perfil</title>
 	<link rel="stylesheet" type="text/css" href="css/edicao.css"/>
 	<link rel="icon" type="imagem/png" href="/img/iPettt.png" />
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
 </head>
 <body>
 	<!-- navbar -->
-<?php include 'header.php'; ?>
+	<?php include 'header.php'; ?>
 
-			<section class="titulo">
-				<h1>MEU(S) PET(S)</h1>
-			</section>
+	<section class="titulo">
+		<h1>MEU(S) PET(S)</h1>
+	</section>
 
-			<h3>altera informações do perfil <a href="alterar-perfil-ong.php?id=<?= $_SESSION['id_ong']?>">Alterar</a></h3>
-			<section class="edc">
-				<div class="box">
-					<div class="box1">
+	<h3>Alterar informações do perfil <a href="alterar-perfil-ong.php?id=<?= $_SESSION['id_ong']?>">Alterar</a></h3>
+	<section class="edc">
+		<div class="box">
+			<div class="box1">
 
 
-						<?php if ($rowTotal > 0 && isset($_SESSION['id_ong'])): ?>
+				<?php if ($rowTotal > 0): ?>
+					<?php foreach ($animais as  $animal): ?>
+						<?php
+						$imagem = 'imagens/1/bbb.jpeg';
+						if (is_file("imagens/" .  $animal['ANI_CODIGO'] . "/" . $animal['ANI_IMAGEM'])) {
+							$imagem = "imagens/" .  $animal['ANI_CODIGO'] . "/" . $animal['ANI_IMAGEM'];
+						}
+						$stmt2 = $pdo->prepare("SELECT COUNT(*) AS total FROM IPET_LIKE WHERE LIK_ANI_CODIGO = ?");
 
-							<?php foreach ($animais as  $animal): ?>
-								<?php
-								$imagem = 'imagens/1/bbb.jpeg';
-								if (is_file("imagens/" .  $animal['ANI_CODIGO'] . "/" . $animal['ANI_IMAGEM'])) {
-									$imagem = "imagens/" .  $animal['ANI_CODIGO'] . "/" . $animal['ANI_IMAGEM'];
-								}
-								?>
-								<div class="box2">
-									<dl>
-										<dt>Chave ong</dt>
-										<dd><?= $animal['ANI_ONG_ID']?></dd>
-										<dt>Nome</dt>
-										<dd><?= $animal['ANI_NOME']?></dd>
-										<dd><img src="<?= $imagem ?>"></dd>
-										<dt>Espécie</dt>
-										<dd><?= $animal['ANI_ESPECIE']?></dd>
-										<dt>Raça</dt>
-										<dd><?= $animal['ANI_RAÇA']?></dd>
-										<dt>Porte</dt>
-										<dd><?= $animal['ANI_PORTE']?></dd>
-										<dt>Gênero</dt>
-										<dd><?= $animal['ANI_GENERO']?></dd>
-										<dt>Descrição</dt>
-										<dd><?= $animal['ANI_DESCRICAO']?></dd>
-									</dl>
-									<br>
-									<a href="edicao_animal.php?id=<?= $animal['ANI_CODIGO'] ?>">Editar</a>
-									<a href="" style="background: red;">Excluir</a>
+						$stmt2->execute([$animal['ANI_CODIGO']]);
+						$likes =  $stmt2->fetch();
+						?>
+
+						<div class="ui card">
+							<div class="image">
+								<img src="<?= $imagem ?>">
+							</div>
+
+							<div class="content">
+								<div class="header"><?= $animal['ANI_NOME']?></div>
+								<div class="summary">
+									Responsável:
+									<?php if ($animal['ANI_NOR_CODIGO'] != null): ?>
+										<a href="perfil-usuario.php?id=<?= $animal['NOR_CODIGO'] ?>"><?= $animal['NOR_NOME']?></a>
+										<?php elseif($animal['ANI_ONG_ID'] != null): ?>
+											<a href="perfil-ong-geral.php?id=<?= $animal['ONG_ID'] ?>"><?= $animal['ONG_NOME']?></a>
+										<?php endif ?>
+									</div>
 								</div>
-								
 
-							<?php endforeach; ?>
-						<?php endif; ?>
+								<div class="extra content">
+									<div class="description">
+										DESCRIÇÃO <br>
+										Espécie: <?= $animal['ESP_ESPECIE']?> <br>
+										Raça: <?= $animal['ANI_RAÇA']?> <br>
+										Porte: <?= $animal['ANI_PORTE']?> | Gênero: <?= $animal['ANI_GENERO']?> <br>
+									</div>
+								</div>
 
-					</div>
+								<div class="extra content">
+									<div class="descriptin">
+										Sobre o pet: <?= $animal['ANI_DESCRICAO']?>
+									</div>
+								</div>
+								<a href="edicao_animal.php?id=<?= $animal['ANI_CODIGO'] ?>">EDITAR<a href="delete.php?id=<?= $animal['ANI_CODIGO'] ?>">EXCLUIR</a>
+
+							</div>
+
+						<?php endforeach; ?>
+					<?php endif; ?>
+
 				</div>
 			</section>
 			<?php include 'footer.php'; ?>
@@ -101,7 +118,3 @@ $rowTotal = $stmt->rowCount();
 	</script>
 </body>
 </html>
-
-
-
- ?>
